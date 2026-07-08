8d6c07ec516b3bf02f76988d40d6bf68f4ff4855
rdolghiu <rdolghiu@gmail.com>
2026-07-07 20:37:18 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,41 +1,23 @@
-import numpy as np
-import pandas as pd
 import regex as re
-import joblib
-import en_core_web_sm
-
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.svm import LinearSVC
-
-nlp = en_core_web_sm.load()
-classifier = LinearSVC()
 
 def clean_text(text):
-    # reduce multiple spaces and newlines to only one
-    text = re.sub(r'(\s\s+|\n\n+)', r'\1', text)
+    # reduce multiple spaces and newlines
+    text = re.sub(r'\s+', ' ', text)
     # remove double quotes
     text = re.sub(r'"', '', text)
+    return text.strip()
 
-    return text
-	
 def convert_text(text):
-    sent = nlp(text)
-    ents = {x.text: x for x in sent.ents}
-    tokens = []
-    for w in sent:
-        if w.is_stop or w.is_punct:
-            continue
-        if w.text in ents:
-            tokens.append(w.text)
-        else:
-            tokens.append(w.lemma_.lower())
-    text = ' '.join(tokens)
-
-    return text
-
+    # lowercase
+    text = text.lower()
+    # remove punctuation
+    text = re.sub(r'[^a-z0-9\s]', '', text)
+    # collapse spaces
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
 
 class preprocessor(TransformerMixin, BaseEstimator):
-
     def __init__(self):
         pass
 
